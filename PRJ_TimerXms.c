77b10c8ca511009b2fe136e6cd9df927bd8aebc0
neamtiu <32874549+neamtiu@users.noreply.github.com>
2017-10-18 23:27:17 +0300

    Add files via upload

---
+++ PRJ_TimerXms.c
@@ -0,0 +1,33 @@
+/*
+ * PRJ_TimerXms.c
+ *
+ * Created: 18.10.2017 22:31:43
+ * Author : Dani
+ */ 
+
+#include "PRJ_TimerXms.h"
+    
+void PRJ_TimerXms(int value_ms)
+{
+	int OCRvalue;
+	//Use CTC on OCR1A (working mode4) for Timer1(16 bit)
+	TCCR1B |= (1 << WGM12);
+	
+	//Period = 0,064ms (for 16Mhz clock and 1024 prescaler value) 
+	//=> 15625d(3D09h) impulses for 1 second 
+	   
+	OCRvalue = value_ms / 0.064;
+	OCR1A = OCRvalue;
+	
+	//Set the prescaler at 1024 + start the timer(starts when prescaler is active)
+	TCCR1B |= (1<<CS12) | (1<<CS10);
+	
+	while(!TIFR1)
+	{
+		//waits for overflow (passed 1 second)
+	}
+	
+	//stop the timer
+	TCCR1B &= 0b11111000;	
+}
+
---
+++ PRJ_TimerXms.h
@@ -0,0 +1,16 @@
+/*
+ * PRJ_TimerXms.h
+ *
+ * Created: 18.10.2017 22:34:48
+ *  Author: Dani
+ */ 
+
+
+#ifndef PRJ_TIMERXMS_H_
+#define PRJ_TIMERXMS_H_
+
+#include <avr/io.h>
+
+void PRJ_mytimer_ms(int value_ms);
+
+#endif /* PRJ_TIMERXMS_H_ */
