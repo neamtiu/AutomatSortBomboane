9770490577e7eb8305bab1478dd84028bd90a763
neamtiu <32874549+neamtiu@users.noreply.github.com>
2017-10-19 16:53:41 +0300

    Update PRJ_Blink.c

--- PRJ_Blink.c
+++ PRJ_Blink.c
@@ -7,7 +7,7 @@
 
 #include "PRJ_Blink.h"
 
-void PRJ_Blink(uint8_t PB_pinSelect)
+void PRJ_BlinkLed(uint8_t PB_pinSelect)
 {
 	DDRB |= (1<<PB_pinSelect);//put the specified pin in output mode
 	PORTB |= (1<<PB_pinSelect);//set the specified pin
