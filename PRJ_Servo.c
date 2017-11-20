c3e91a2896b8a91105f41a27e45cdd8e4f1014ad
neamtiu <32874549+neamtiu@users.noreply.github.com>
2017-11-20 18:46:18 +0200

    Add files via upload

---
+++ PRJ_Servo.c
@@ -0,0 +1,31 @@
+/*
+ * PRJ_Servo.c
+ *
+ * Created: 31.10.2017 23:29:25
+ * Author : Dani
+ */ 
+
+
+void PRJ_Servo(uint16_t position)
+{
+	//reset timer control registers
+	TCCR1A = 0;
+	TCCR1B = 0;
+	ICR1 = 0;
+	//reset the timer
+	TCNT1 = 0;
+	//set port b pin 1 as pwm output
+	DDRB |= 0xFF;
+	//use timer1 in fast pwm mode using ICR1 as TOP
+	TCCR1A |= (1 << WGM11);
+	TCCR1B |= (1 << WGM12)|(1 << WGM13);
+	//use pwm in non-inverted mode
+	TCCR1A |= (1 << COM1A1);
+	//set top value 39999d (20ms)
+	ICR1 = 39999;
+	//set OCR1A in order to go to the specified position
+	OCR1A = position;
+	//set prescaler at 8 and start the timer
+	TCCR1B|=  (1<<CS11);
+}
+
---
+++ PRJ_Servo.h
@@ -0,0 +1,18 @@
+/*
+ * PRJ_Servo.h
+ *
+ * Created: 31.10.2017 23:29:55
+ *  Author: Dani
+ */ 
+
+
+#ifndef PRJ_SERVO_H_
+#define PRJ_SERVO_H_
+
+#include <avr/io.h>
+
+#define Middle 3000	//pulses
+#define Right 2000 //pulses
+#define Left 4000 //pulses
+
+#endif /* PRJ_SERVO_H_ */
